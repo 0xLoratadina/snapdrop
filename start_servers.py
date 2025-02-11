51408540350bed02dea7c40effee6d75d5287e4f
userxboy <88602948+DrBabaBoy@users.noreply.github.com>
2025-02-11 13:27:08 -0600

    Update start_servers.py

--- start_servers.py
+++ start_servers.py
@@ -2,7 +2,6 @@ import subprocess
 import threading
 import time
 
-# Función para mostrar la salida en tiempo real de cada proceso
 def stream_output(process, name):
     for line in iter(process.stdout.readline, b''):
         if line:
@@ -11,7 +10,7 @@ def stream_output(process, name):
 def start_pyro_naming_server():
     print("Iniciando el servidor de nombres de Pyro4...")
     naming_server_process = subprocess.Popen(['python', '-m', 'Pyro4.naming'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    time.sleep(3)  # Dar tiempo para que el servidor de nombres inicie correctamente
+    time.sleep(3)  
     return naming_server_process
 
 def start_pyro_server():
@@ -26,22 +25,18 @@ def start_flask_server():
 
 def main():
     try:
-        # Iniciar el servidor de nombres de Pyro4
+
         naming_server_process = start_pyro_naming_server()
 
-        # Iniciar el servidor Pyro4
         pyro_server_process = start_pyro_server()
 
-        # Iniciar el servidor Flask
         flask_server_process = start_flask_server()
 
         print("Todos los servidores se han iniciado correctamente.")
 
-        # Crear hilos para mostrar la salida de cada servidor
         threading.Thread(target=stream_output, args=(pyro_server_process, "Pyro4"), daemon=True).start()
         threading.Thread(target=stream_output, args=(flask_server_process, "Flask"), daemon=True).start()
 
-        # Mantener los procesos corriendo
         while True:
             time.sleep(1)
     
